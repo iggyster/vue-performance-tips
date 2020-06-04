0cf8fae7b0d400e0a6396c2735851e594391f911
Igor Pronin <igor.pronin@ddi-dev.com>
2020-06-04 14:01:39 +0300

    Update main entry
    Make App component cache router view (optimization).
    Add bootstrap to quickly create layouts. Define own theme to rewrite bootstrap defaults.
    Create plugins entry to separate peer imports from main entry.

---
+++ src/plugins.js
@@ -0,0 +1,10 @@
+import(
+    /* webpackChunkName: "jquery" */
+    /* webpackPrefetch: true */
+    'jquery'
+)
+import(
+    /* webpackChunkName: "popper" */
+    /* webpackPrefetch: true */
+    'popper.js'
+)
---
+++ src/theme/index.js
@@ -0,0 +1,5 @@
+import(
+    /* webpackChunkName: "theme" */
+    /* webpackPrefetch: true */
+    './styles.scss'
+)
