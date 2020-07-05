99192ba3da434038b2a1a89397626c4074504ac1
iggyster <iggyster@users.noreply.github.com>
2020-07-06 00:40:10 +0300

    Add local var benchmark

--- src/components/Benchmarks/LocalVar/LocalVarOn.vue
+++ src/components/Benchmarks/LocalVar/LocalVarOn.vue
@@ -1,11 +1,54 @@
 <template>
-    <div>
-        Local var on
+    <div class="circle">
+        <div v-if="value" class="on">
+            {{ result }}
+        </div>
+        <div v-else class="off">
+            {{ result }}
+        </div>
     </div>
 </template>
 
 <script>
+    function heavy(factor) {
+        return Math.sqrt(Math.cos(Math.sin(factor))) + factor + factor * factor + factor * 2 + factor * 3;
+    }
+
     export default {
-        name: "LocalVarOn"
+        name: "LocalVarOn",
+        props: ['value'],
+        computed: {
+            factor() {
+                return 1000
+            },
+            result ({factor, value}) {
+                let result = value
+                for (let i = 0; i < 1000; i++) {
+                    result += heavy(factor);
+                }
+                return result
+            },
+        },
     }
 </script>
+
+<style scoped>
+    .circle {
+        float: left;
+        width: 21px;
+        height: 21px;
+        margin: 2px;
+    }
+    .on, .off {
+        width: 100%;
+        height: 100%;
+        color: transparent;
+        border-radius: 50%;
+    }
+    .on {
+        background-color: burlywood;
+    }
+    .off {
+        background-color: #2c3e50;
+    }
+</style>
