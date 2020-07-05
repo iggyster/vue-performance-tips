9f056aca6ecea5910496e42487436a367bfe6a06
iggyster <iggyster@users.noreply.github.com>
2020-07-05 19:45:16 +0300

    Add child component benchmark

--- src/components/Benchmarks/Child/ChildOn.vue
+++ src/components/Benchmarks/Child/ChildOn.vue
@@ -1,11 +1,55 @@
 <template>
-    <div>
-        Child on
+    <div class="circle">
+        <div v-if="value" class="on">
+            <Child/>
+        </div>
+        <div v-else class="off">
+            <Child/>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "ChildOn"
-    }
+        name: "ChildOn",
+        props: ['value'],
+        components: {
+            Child: {
+                methods: {
+                    heavy () {
+                        const n = 100000
+                        let result = 0
+                        for (let i = 0; i < n; i++) {
+                            result += Math.sqrt(Math.cos(Math.sin(42)))
+                        }
+                        return result
+                    },
+                },
+                render (h) {
+                    return h('div', this.heavy())
+                },
+            },
+        },
+    };
 </script>
+
+<style scoped>
+    .circle {
+        float: left;
+        width: 21px;
+        height: 21px;
+        margin: 2px;
+    }
+    .on, .off {
+        width: 100%;
+        height: 100%;
+        color: transparent;
+        border-radius: 50%;
+    }
+    .on {
+        background-color: rebeccapurple;
+    }
+    .off {
+        background-color: white;
+    }
+</style>
--- src/views/Child.vue
+++ src/views/Child.vue
@@ -1,29 +1,47 @@
 <template>
     <Page>
+        <template #content>
+            <div class="row m-0 justify-content-center">
+                <h1>Child component</h1>
+            </div>
+        </template>
+        <template #switches>
+            <div class="custom-control custom-switch">
+                <input type="checkbox" class="custom-control-input" id="play" v-model="play">
+                <label class="custom-control-label" for="play">Play</label>
+            </div>
+        </template>
         <template #off>
-            <ChildOff/>
+            <div class="grid">
+                <ChildOff v-for="(n,index) of list" :key="index" :value="n"/>
+            </div>
         </template>
         <template #on>
-            <ChildOn/>
+            <div class="grid">
+                <ChildOn v-for="(n,index) of list" :key="index" :value="n"/>
+            </div>
         </template>
     </Page>
 </template>
 
 <script>
+    import generate from "../mixins/generate";
     import ChildOff from "../components/Benchmarks/Child/ChildOff";
     import ChildOn from "../components/Benchmarks/Child/ChildOn";
 
     export default {
         name: "Child",
+        mixins: [generate],
         components: {
             ChildOff,
             ChildOn,
         },
-        props: {
-            optimized: {
-                type: Boolean,
-                default: false,
-            },
-        },
     }
 </script>
+
+<style scoped>
+    .grid {
+        margin: 24px auto;
+        max-width: 800px;
+    }
+</style>
