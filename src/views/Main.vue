5f2e27cf4ada3406b764cce72714649118f024e7
iggyster <iggyster@users.noreply.github.com>
2020-07-14 14:35:33 +0300

    Update main view
    * Remove page usage
    * Centralize logo

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,7 @@
 <template>
-    <Page>
-        <template #content>
-            <div class="row m-0 justify-content-md-center align-items-center">
-                <img src="../assets/logo.png" alt="Vue logo">
-            </div>
-        </template>
-    </Page>
+    <div class="row m-0 justify-content-md-center align-items-center">
+        <img src="../assets/logo.png" alt="Vue logo">
+    </div>
 </template>
 
 <script>
@@ -13,3 +9,9 @@
         name: "Main",
     }
 </script>
+
+<style scoped>
+    div {
+        height: 600px;
+    }
+</style>
